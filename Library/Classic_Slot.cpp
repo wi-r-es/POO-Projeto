d23026f13a68c2281a91ff5038e4ba8acb35d040
wi-r-es <josemachado74@gmail.com>
2024-01-29 23:43:55 +0000

    play method changed

--- Library/Classic_Slot.cpp
+++ Library/Classic_Slot.cpp
@@ -6,13 +6,11 @@
 
 ClassicSlot::ClassicSlot(int posX, int posY) : Machine(MACHINE_TYPE::CLASSIC_SLOT, posX, posY) {
     // Initialize the symbols for this classic slot machine
-    // static_cast<int>(MACHINE_TYPE::CLASSIC_SLOT);
     numSpins = 0;
 }
 
 ClassicSlot::ClassicSlot() : Machine(MACHINE_TYPE::CLASSIC_SLOT, 0,0) {
     // Initialize the symbols for this classic slot machine
-    // static_cast<int>(MACHINE_TYPE::CLASSIC_SLOT);
     numSpins = 0;
 }
 
@@ -62,13 +60,14 @@ bool ClassicSlot::Play(User *user) {
         return true;
     }
 
-    int rol1 = rand() % 4; //randomly a number that represents the 4 simbols
-    int rol2 = rand() % 4;
-    int rol3 = rand() % 4;
+    int rol1 = randomNumberGeneratorInterval(1,4); //randomly a number that represents the 4 simbols
+    int rol2 = randomNumberGeneratorInterval(1,4);
+    int rol3 = randomNumberGeneratorInterval(1,4);
+    int rol4 = randomNumberGeneratorInterval(1,4);
     setBetAmount(userMoney <= 5 ? 1 : static_cast<float>(randomNumberGeneratorInterval(5, static_cast<int>(userMoney))));
     user->setMoney(userMoney - getBetAmount());
     user->incBets(1);
-    if (rol1 == 7 && rol2 == 7 && rol3 == 7) {
+    if (rol1==rol2==rol3==rol4) {
         auto profit = getBetAmount()*12;
         user->setMoney(userMoney + profit);
         user->incPrizesWon(profit);
@@ -76,7 +75,7 @@ bool ClassicSlot::Play(User *user) {
         auto elapsed = chrono::duration_cast<chrono::seconds>(start_time - end_time);
         user->incTimeSpent(elapsed);
         return true;
-    } else if (rol1 == rol2 && rol2 == rol3) {
+    } else if (rol1==rol2 || rol1==rol3 || rol1==rol4 || rol2==rol3 || rol2==rol4 || rol3==rol4 ) {
         auto profit = getBetAmount()*4;
         user->setMoney(userMoney + profit);
         user->incPrizesWon(profit);
